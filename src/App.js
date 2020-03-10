53e9206cc5f7df6f8e915492d4f5b060d8895e62
J2P <jjp5023@gmail.com>
2020-03-11 00:42:23 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [address, setAddress] = useState('');
+  const [data, setData] = useState();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const url = `https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByAddr/json?address=${address}`;
+    const response = await axios.get(url);
+    const stores = response.data.stores.filter(store => (
+      store.remain_stat === 'plenty' || store.remain_stat === 'some' || store.remain_stat === 'few'
+    ))
+    setData(stores);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} placeholder="경기도 남양주시 별내동" />
+      </form>
+      {!data && <div>주소를 바르게 입력해 주세요.</div>}
+      {data && <ul>
+        {data.map((store, index) => (
+          <li key={index}>
+            <h2>
+              {store.name}
+              {store.remain_stat === 'plenty' && <span className="green">100개 이상</span>}
+              {store.remain_stat === 'some' && <span className="yellow">30개 이상 100개미만</span>}
+              {store.remain_stat === 'few' && <span className="red">2개 이상 30개 미만</span>}
+              {store.remain_stat === 'empty' && <span className="gray">1개 이하</span>}
+            </h2>
+            <p>{store.addr}</p>
+            <p>입고 시간: {store.stock_at}</p>
+          </li>
+        ))}
+      </ul>}
     </div>
   );
 }
