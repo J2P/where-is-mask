d726d52aaa776b94ec96c629fb699535c7e87377
J2P <jjp5023@gmail.com>
2020-03-11 13:56:03 +0900

    add quick search

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import axios from 'axios';
 
 function App() {
   const [address, setAddress] = useState('');
-  const [data, setData] = useState();
+  const [data, setData] = useState(undefined);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setData(undefined);
     const url = `https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByAddr/json?address=${address}`;
     const response = await axios.get(url);
     const stores = response.data.stores
@@ -15,6 +16,11 @@ function App() {
     const some = stores.filter(store => store.remain_stat === 'some')
     const few = stores.filter(store => store.remain_stat === 'few')
     setData([...plenty,...some,...few]);
+  };
+
+  const handleQuickSubmit = (e, addr) => {
+    setAddress(addr);
+    handleSubmit(e);
   }
 
   return (
@@ -22,7 +28,12 @@ function App() {
       <form onSubmit={handleSubmit}>
         <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} placeholder="경기도 남양주시 별내동" />
       </form>
-      {!data && <div>주소를 바르게 입력해 주세요.</div>}
+      <div>
+        <span className="label" onClick={(e) => handleQuickSubmit(e, '경기도 남양주시 별내동')}>별내동</span>
+        <span className="label" onClick={(e) => handleQuickSubmit(e, '서울특별시 중랑구 신내동')}>신내동</span>
+        <span className="label" onClick={(e) => handleQuickSubmit(e, '대전광역시 서구 둔산동')}>둔산동</span>
+        <span className="label" onClick={(e) => handleQuickSubmit(e, '대전광역시 서구 탄방동')}>탄방동</span>
+      </div>
       {data && <ul>
         {data.map((store, index) => (
           <li key={index}>
